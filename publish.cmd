67464b92d77a7535dc3292c424652bee6ea9b4a1
KrutiGuptaA <krutigupta2711@gmail.com>
2018-10-10 19:17:21 +0530

    azuredeploy
    added a json file

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=armbotsample-Web-Deploy.pubxml -p:Password=oo7wbCkiSMxf6gg6WoExXi381wys3xaal5o8tnCHJvEPnw0GdshNZvHm4alj
+
